e582e1eb1239187deaf328f35065a43e86f06221
guban <gubanx@gmail.com>
2017-12-10 23:54:19 +0100

    Controller index

---
+++ src/main/java/org/sample/recipe/IndexController.java
@@ -0,0 +1,15 @@
+package org.sample.recipe;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping({"", "/", "/index"})
+    public String index(Model model) {
+        System.out.println("----------- 11111");
+        return "index";
+    }
+}
