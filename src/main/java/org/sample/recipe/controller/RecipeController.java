ce6d4dc439c29a9e9bab560a14bc4fd683325913
guban <gubanx@gmail.com>
2017-12-20 23:55:00 +0100

    Crear receta

--- src/main/java/org/sample/recipe/controller/RecipeController.java
+++ src/main/java/org/sample/recipe/controller/RecipeController.java
@@ -1,9 +1,12 @@
 package org.sample.recipe.controller;
 
+import org.sample.recipe.command.RecipeCommand;
 import org.sample.recipe.service.RecipeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -19,5 +22,18 @@ public class RecipeController {
     public String showById(@PathVariable String id, Model model) {
         model.addAttribute("recipe", recipeService.findById(Long.valueOf(id)));
         return "recipe/show";
+    }
+
+    @RequestMapping("/recipe/new")
+    public String newRrecipe(Model model) {
+        model.addAttribute("recipe", new RecipeCommand());
+        return "recipe/recipe_form";
+    }
+
+    @PostMapping
+    @RequestMapping("/recipe")
+    public String saveOrUpdate(@ModelAttribute RecipeCommand recipeCommand) {
+        RecipeCommand savedRecipe = recipeService.saveRecipeCommand(recipeCommand);
+        return "redirect:/recipe/show/" + savedRecipe.getId();
     }
 }
